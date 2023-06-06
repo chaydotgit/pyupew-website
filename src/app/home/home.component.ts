61db8aed542d0c91007b625c0a049c6792ff7172
Chayanne Rodriguez <chayanne624@gmail.com>
2023-06-05 17:49:09 -0700

    added process and template to commissions, changed up format

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal, NgbModal } from "@ng-bootstrap/ng-bootstrap";
+
+@Component({
+  selector: 'contact-modal',
+  template: `
+    <div class="modal-header">
+      <div class="modal-title">Contact</div>
+      <button type="button" class="btn-close" aria-label="Close" (click)="activeModal.dismiss('Cross click')"></button>
+    </div>
+    <div class="modal-body text-center">
+      Contact me through the information below!
+      <div class="social-links">
+        <i class="bi bi-envelope-heart"> hazelsolares@gmail.com</i>
+        <i class="bi bi-discord"> @pyu#7518</i>
+      </div>
+
+    </div>
+  `,
+})
+
+export class ContactModalContent {
+  constructor(public activeModal: NgbActiveModal) {
+  }
+}
 
 @Component({
   selector: 'app-home',
@@ -7,9 +31,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
+  }
+
+  open() {
+    this.modalService.open(ContactModalContent);
   }
 
 }
